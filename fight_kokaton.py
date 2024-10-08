67e3b52742e1becab9dadde61ca9cd5ad3e1b6fe
C0B23021 <c0b23021c5@edu.teu.ac.jp>
2024-10-08 14:45:03 +0900

    喜ぶコウカトン

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -168,10 +168,11 @@ def main():
                 return
             
         if beam is not None:
-                if bomb is not None:
-                    if beam.rct.colliderect(bomb.rct):  # ビームと爆弾が衝突したら
-                        beam, bomb = None, None
-                        pg.display.update()
+            if bomb is not None:
+                if beam.rct.colliderect(bomb.rct):  # ビームと爆弾が衝突したら
+                    beam, bomb = None, None
+                    bird.change_img(6, screen)
+                    pg.display.update()
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
